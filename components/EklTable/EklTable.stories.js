06b007ea9ec6da8c99fe11e50040fd7feb4fd08c
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2019-08-30 17:07:17 +0300

    Add EklTable

---
+++ components/EklTable/EklTable.stories.js
@@ -0,0 +1,26 @@
+import { storiesOf } from '@storybook/vue'
+import EklTable from './EklTable'
+
+var items = [
+    ['Yair Lamb', 'yair.lamb@email.com'],
+    ['Leonardo Payne', 'leonardo.payne@email.com'],
+    ['Carl Henson', 'carl.henson@email.com'],
+    ['Jensen Combs', 'jensen.combs@email.com'],
+    ['Amiah Burton', 'amiah.burton@email.com'],
+    ['Yaretzi Mayo', 'yaretzi.mayo@email.com'],
+    ['Kamren Huffman', 'kamren.huffman@email.com']
+]
+
+storiesOf('EklTable', module)
+    .add('with 7 items', () => ({
+        components: { EklTable },
+        template:
+            `
+      <EklTable
+        :items="items"
+      />
+    `,
+        data: () => ({
+            items
+        })
+    }))
---
+++ components/EklTable/EklTable.vue
@@ -0,0 +1,21 @@
+<template>
+  <table>
+    <EklTableRow v-for="(values, index) in items" :key="index" :values="values" />
+  </table>
+</template>
+
+<script>
+import EklTableRow from "@eklogvinov/ekl-table-row/EklTableRow";
+export default {
+  name: "EklTable",
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    EklTableRow
+  }
+};
+</script>
